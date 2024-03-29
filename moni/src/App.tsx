7a3f68ab04efa86fe0fb488c18b0579b838e3a2b
Hugo Vinicius <hugovenicius@hotmail.com.br>
2024-03-29 18:32:34 -0300

    Pequenas mudanças

--- moni/src/App.tsx
+++ moni/src/App.tsx
@@ -4,16 +4,17 @@ import { NovaSala } from "./pages/NovaSala";
 import { PaginaInicial } from "./pages/PaginaInicial";
 import { Sala } from "./pages/Sala";
 
-export default function App() {
+function App() {
   return (
     <BrowserRouter>
       <AutenticacaoProvider>
         <Routes>
           <Route path="/" element={<PaginaInicial />} />
-          <Route path="/rooms/new" element={<NovaSala />} />
-          <Route path="/rooms/:id" element={<Sala />} />
+          <Route path="/salas/nova" element={<NovaSala />} />
+          <Route path="/salas/:id" element={<Sala />} />
         </Routes>
       </AutenticacaoProvider>
     </BrowserRouter>
   );
 }
+export default App;
